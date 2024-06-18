6f1d7a85e996e0f6b4f4685383a1518a7c1e8d8d
kisandas13 <45478086+kamal17121995@users.noreply.github.com>
2024-06-18 19:59:20 +0530

    plusMinus problem solved from hackerRank

---
+++ 06-18-2024/01-plusMinus.js
@@ -0,0 +1,50 @@
+// Plus Minus
+
+// Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with
+
+// places after the decimal.
+
+// Note: This challenge introduces precision problems. The test cases are scaled to six decimal places, though answers with absolute error of up to
+// are acceptable.
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function plusMinus(arr) {
+    var mas=0,men=0, cer=0;
+    const n= arr.length;
+    
+    arr.map(el=> el >0 ? mas+=1/n :(el<0 ? men+=1/n : cer+=1/n) )
+    return console.log(mas.toFixed(6),"\n" ,men.toFixed(6),"\n" , cer.toFixed(6))
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    plusMinus(arr);
+}
